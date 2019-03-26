b9868dad6147e609619c5d0c920fa54960644d70
Dmitry <dmpuchenkov@gmail.com>
2019-03-26 14:27:22 +0300

    Done

--- 2/main.cpp
+++ 2/main.cpp
@@ -1,6 +1,5 @@
-/*Пученков Дмитрий
+/* Пученков Дмитрий
  * АПО-13
- *
  * Даны два массива неповторяющихся целых чисел, упорядоченные по возрастанию.
  * A[0..n-1] и B[0..m-1]. n » m. Найдите их пересечение.
  * Требуемое время работы: O(m * log k), где k - позиция элементта B[m-1] в массиве A..
@@ -8,7 +7,7 @@
  */
 #include <iostream>
 
-int ElemCheck(const int *firstarr, int first, int elem, int numeral);
+int BinarySearch(int *arr, int size, int elem, int left = 0);
 
 int main() {
     int first;
@@ -19,19 +18,31 @@ int main() {
     int SecondArr[second];
     for (int i = 0; i < first; i++)
         std::cin >> FirstArr[i];
-    for (int i = 0; i < second; i++) {
+    std::cin >> SecondArr[0];
+    tmp[0] = BinarySearch(FirstArr, first, SecondArr[0]);
+
+    for (int i = 1; i < second; i++) {
         std::cin >> SecondArr[i];
-        tmp[i] = ElemCheck(FirstArr, first, SecondArr[i], i);
+        tmp[i] = BinarySearch(FirstArr, first, SecondArr[i], tmp[i - 1]);
     }
     for (int i = 0; i < second; i++)
         if (tmp[i] != -1)
-            std::cout << tmp[i] << " ";
+            std::cout << FirstArr[tmp[i]] << " ";
     return 0;
 }
 
-int ElemCheck(const int *firstarr, int first, int elem, int numeral) {
-    for (numeral; numeral < first; numeral++)
-        if (elem == firstarr[numeral])
-            return elem;
-    return -1;
+int BinarySearch(int *arr, int size, int elem, int left) {
+    int right = size;
+    while (true) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == elem)
+            return mid;
+        if (arr[mid] <= elem)
+            left = mid + 1;
+        else
+            right = mid;
+        if (right == left)
+            return -1;
+    }
+
 }
