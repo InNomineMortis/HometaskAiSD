4619fd9e8a1e3786c5eee82a0ca3a30d5da7e88d
Dmitry <dmpuchenkov@gmail.com>
2019-04-06 11:28:40 +0300

    Added grow + made Hash class

--- Module_2/1/main.cpp
+++ Module_2/1/main.cpp
@@ -12,17 +12,49 @@
 #include <vector>
 #include <string.h>
 
+const size_t DEFAULT_SIZE = 8;
+const size_t RESIZE_FACTOR = 2;
+const size_t MAX_ALPHA = 3;
+
 template<typename T>
-struct HeapNode {
+struct HashNode {
+    HashNode() : nextNode(nullptr) {};
+
+    HashNode(const T &key, HashNode<T> *next)
+            : element(key), nextNode(next) {}
+
     std::string element;
     T *nextNode;
 };
+
+size_t Hash(const std::string &s, size_t size) {
+    size_t hash = 0;
+    for (size_t i = 0; i < s.size(); i++) {
+        hash = hash * 73 + s[i];
+    }
+    return hash % size;
+}
 
 template<typename T>
 class HashTable {
 public:
     bool Add(T &elem) {
+        if (size > MAX_ALPHA * table.size())
+            grow();
 
+        size_t hash = Hash(elem, table.size());
+
+        HashNode<T> *node = table[hash];
+        while (node != nullptr && node->key != elem) {
+            node = node->next;
+        }
+
+        if (node != nullptr)
+            return false;
+
+        table[hash] = new HashNode<T>(elem, table[hash]);
+        size++;
+        return true;
     }
 
     bool Delete(T &elem) {
@@ -31,6 +63,28 @@ public:
 
     bool Has(T &elem) {
 
+    }
+
+private:
+    std::vector<HashNode<T> *> table;
+    size_t size;
+    void grow()
+    {
+        std::vector<HashNode<T>*> newTable(table.size() * RESIZE_FACTOR, nullptr);
+
+        for (size_t i = 0; i < table.size(); i++)
+        {
+            HashNode<T> *node = table[i];
+            while (node != nullptr)
+            {
+                size_t newHash = Hash(node->key, newTable.size());
+                HashNode<T> *tmp = node->next;
+                node->next = newTable[newHash];
+                newTable[newHash] = node;
+                node = tmp;
+            }
+        }
+        table = std::move(newTable);
     }
 };
 
